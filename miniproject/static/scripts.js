d826a0f5dc78aff6037a2c024e07224ca962b7e7
홍인화 <happinhwa@happinhwa-MacBook-Pro.local>
2023-05-16 10:05:28 +0900

    로그인, 회원가입 디자인

--- miniproject/static/scripts.js
+++ miniproject/static/scripts.js
@@ -5,3 +5,14 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// function checkPhoneFormat(input) {
+//     var phoneNum = input.value.replace(/-/g, '');
+//     var phoneNumRegex = /^[0-9]{11}$/; // 11자리 숫자
+//     if (!phoneNumRegex.test(phoneNum)) {
+//         alert('올바른 전화번호 형식이 아닙니다.');
+//         input.focus();
+//     }
+// }
+
+
